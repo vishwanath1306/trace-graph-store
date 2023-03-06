b865aab9d6320d2199d08ddd5832f8a60550c401
Vishwanath Seshagiri <vishwa.nath@outlook.com>
2023-03-06 15:27:13 -0500

    Added json files

--- base_storage/src/data_structure.rs
+++ base_storage/src/data_structure.rs
@@ -1,19 +1,76 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Personal{
-    pub name: String,
-    pub goal: String,
-    pub age: u8,
-    pub visited: Vec<String>,
-    pub extra: Option<String>
+#[serde(untagged)]
+pub enum TagValue {
+    Float(f64),
+    String(String),
 }
 
-// #[derive(Debug, Clone, Serialize, Deserialize)]
-// pub struct JaegerRoot{
-//     pub data: Vec<>,
-//     pub total: u64,
-//     pub limit: u64,
-//     pub offset: u64, 
-//     pub errors: Option<>
-// }
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct References{
+    #[serde(rename(deserialize = "refType"))]
+    pub ref_type: String,
+
+    #[serde(rename(deserialize = "traceID"))]
+    pub trace_id: String,
+
+    #[serde(rename(deserialize = "spanID"))]
+    pub span_id: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Tags{
+    pub key: String,
+
+    #[serde(rename(deserialize = "type"))]
+    pub type_: String,
+
+    pub value: TagValue,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Span{
+
+    #[serde(rename(deserialize = "traceID"))]
+    pub trace_id: String,
+
+    #[serde(rename(deserialize = "spanID"))]
+    pub span_id: String, 
+
+    pub flags: u8,
+
+    #[serde(rename(deserialize = "operationName"))]
+    pub operation_name: String, 
+
+    pub references: Vec<Option<References>>,
+    
+    #[serde(rename(deserialize = "startTime"))]
+    pub start_time: u128,
+
+    pub duration: u128,
+
+    pub tags: Vec<Tags>
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Data{
+    #[serde(rename(deserialize = "traceID"))]
+    pub trace_id: String, 
+
+    pub spans: Vec<Span>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct JaegerRoot{
+    pub data: Vec<Data>,
+
+    pub total: u64,
+
+    pub limit: u64,
+
+    pub offset: u64, 
+
+    pub errors: Option<String>,
+}
--- base_storage/src/lib.rs
+++ base_storage/src/lib.rs
@@ -1,18 +1,13 @@
 pub mod data_structure;
 
 use std::fs;
-use data_structure::Personal;
+use data_structure::JaegerRoot;
 
-pub fn parse_personal_json_file(filename: String) -> Personal{
 
+pub fn jaeger_parser(filename: String) -> JaegerRoot{
     let file_descriptor = fs::File::open(filename).expect("File should be opened in read only");
-    let person_details: Personal = serde_json::from_reader(file_descriptor).expect("ile should be proper JSON");
-    person_details
-}
-
-
-pub fn jaeger_parser(filename: String){
-
+    let jaeger_base: JaegerRoot = serde_json::from_reader(file_descriptor).expect("File should be proper JSON");
+    jaeger_base
 }
 
 
@@ -21,25 +16,11 @@ mod tests {
     use super::*;
 
     #[test]
-    pub fn test_personal_json_parser(){
-        let example_file = "./example_json/example_personal.json";
-        let personal_details = parse_personal_json_file(example_file.to_string());
-        let visited_string = vec!["SFO".to_string(), "NYC".to_string(), "ATL".to_string(), "BOL".to_string()];
-        assert_eq!("John", personal_details.name);
-        assert_eq!(26, personal_details.age);
-        assert_eq!(visited_string, personal_details.visited);
-        assert_eq!(None, personal_details.extra);
+    pub fn test_jaeger_base(){
+        let jaeger_base_file = "./example_json/example_jaeger_trace.json";
+        let jaeger_details = jaeger_parser(jaeger_base_file.to_string());
+        // println!("The jaeger details are: {:?}", jaeger_details);
+        println!("The jaeger data for 1 is: {:?}", jaeger_details.data[0].spans[0]);
+        assert_eq!(0, jaeger_details.total);
     }
-
-    #[test]
-    pub fn test_personal_json_with_string(){
-        let example_file = "./example_json/example_personal_string.json";
-        let visited_string = vec!["SFO".to_string(), "NYC".to_string(), "ATL".to_string(), "BOL".to_string()];
-        let personal_details = parse_personal_json_file(example_file.to_string());
-        assert_eq!("John", personal_details.name);
-        assert_eq!(26, personal_details.age);
-        assert_eq!(visited_string, personal_details.visited);
-        assert_eq!("Hello World", personal_details.extra.unwrap());
-    }
-    
 }
