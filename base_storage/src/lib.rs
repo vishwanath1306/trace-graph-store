1c45e45c9d870ff6c54569e16013ab5735122fae
Vishwanath Seshagiri <vishwa.nath@outlook.com>
2023-04-19 17:19:02 -0400

    Removed wrong db

--- base_storage/src/lib.rs
+++ base_storage/src/lib.rs
@@ -16,23 +16,6 @@ pub fn postgres_bridge(){
     // Figure out the different parameters you need
     // Ideally should be for setting up the 
     let client = Client::connect("postgresql://vishwanath:beyblade@localhost/sample_db", postgres::NoTls);
-    // match client{
-    //     Ok(mut response) => {
-    //         let value = response.batch_execute("
-    //         CREATE TABLE IF NOT EXISTS app_user (
-    //         id              SERIAL PRIMARY KEY,
-    //         username        VARCHAR UNIQUE NOT NULL,
-    //         password        VARCHAR NOT NULL,
-    //         email           VARCHAR UNIQUE NOT NULL
-    //         )").unwrap();
-
-    //         println!("Added new table");
-    //     }, 
-
-    //     Err(_) => {
-    //         panic!("Unable to create client connection.");
-    //     }
-    // }
 }
 #[cfg(test)]
 mod tests {
